f922c1756001128b20fe83f469edb4a1d23e572e
hackerorientado <hackerorientado@protonmail.com>
2022-04-19 19:32:53 -0300

    Alterado arquivos do cursode Rails

--- modulo_03/crypto_wallet/config/routes.rb
+++ modulo_03/crypto_wallet/config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'welcome#index' # É PRECISO INFORMAR O CONTROLLER A AÇÃO QUE ELE IRÁ TOMAR (NO CASO AQUI É O INDEX). POR ISSO NÃO FUNCIONA COLOCAR O HELPER welcome_index_path
 end
