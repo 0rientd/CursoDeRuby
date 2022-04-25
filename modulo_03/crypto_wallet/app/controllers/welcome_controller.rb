a7b985d44778a008ea54169dc56b51ebe69cb69b
hackerorientado <hackerorientado@protonmail.com>
2022-04-25 19:28:36 -0300

    Adicionado o método 'params' para receber informações direto do método de requisição GET. Desta forma alteramos varíaveis no Controller para apresentar nas Views

--- modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @nome = "Carlos"
+    @meu_nome = params[:nome]
+    @meu_curso = params[:curso]
+
+    # URL DE EXEMPLO = http://localhost:3000/?nome=Paula&curso=Ruby%20on%20Rails
+
   end
 end
