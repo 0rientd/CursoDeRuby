f0b241b30810781534f09833a900027e0e9678fe
hackerorientado <hackerorientado@protonmail.com>
2022-04-25 19:04:38 -0300

    Adicionado um helper para retornar na index em qual ambiente estamos desenvolvendo.

--- modulo_03/crypto_wallet/app/helpers/application_helper.rb
+++ modulo_03/crypto_wallet/app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
     def data_br(data_us)
         data_us.strftime("%d/%m/%y")
     end
+
+    def ambiente_rails
+        if Rails.env.development?
+            "desenvolvimento"
+
+        elsif Rails.env.prodction?
+            "produção"
+       
+        else
+            "Teste"
+        end
+    end
 end
