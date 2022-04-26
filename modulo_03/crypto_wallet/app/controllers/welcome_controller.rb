85e1b2ced4a78e1e9b4b7e767e7eff66ec941baf
hackerorientado <hackerorientado@protonmail.com>
2022-04-26 10:19:12 -0300

    Alterado o welcome_controller.rb para que tenha um valor padrão caso não seja passado valores para as variáveis de sessão/instância via URL/GET.

--- modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
+++ modulo_03/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,9 +1,14 @@
 class WelcomeController < ApplicationController
   def index
-    @meu_nome = params[:nome]
-    @meu_curso = params[:curso]
+#    @meu_nome = params[:nome]
+#    @meu_curso = params[:curso]
+
+    if @meu_nome == nil ? (@meu_nome = "Carlos") : (@meu_nome = params[:nome])
+    end
+
+    if @meu_curso == nil ? (@meu_curso = "Ruby on Rails") : (@meu_curso =  params[:curso])
+    end
 
     # URL DE EXEMPLO = http://localhost:3000/?nome=Paula&curso=Ruby%20on%20Rails
-
   end
 end
