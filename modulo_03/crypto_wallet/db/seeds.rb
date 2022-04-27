6219289330dd03324515b99168676a80f085ceff
hackerorientado <hackerorientado@protonmail.com>
2022-04-27 19:22:05 -0300

    Criada as seeds para testar os camandos db:drop:_unsafe, db:create, db:migrate e db:seed

--- modulo_03/crypto_wallet/db/seeds.rb
+++ modulo_03/crypto_wallet/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png"
+)
+
+Coin.create!(
+    description: "Dogecoin",
+    acronym: "Doge",
+    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/74.png"
+)
