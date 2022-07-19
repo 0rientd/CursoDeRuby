879156f706e150a095f5f8c156bfa12527820004
0rientd <chbarros.junior@gmail.com>
2022-07-19 16:12:37 -0300

    ajustado comentário

--- modulo_03/crypto_wallet/db/migrate/20220719190115_add_mining_type_to_coins.rb
+++ modulo_03/crypto_wallet/db/migrate/20220719190115_add_mining_type_to_coins.rb
@@ -1,6 +1,6 @@
 # O Rails lê o comando passado pelo terminal e, caso siga o padrão, ele saberá onde fazer as referências das tabelas.
-# Coins foi passado e ele entende que será adicionado nesta tabela. O 'mining_type' ele sabe que é referência ao model pois eles tem o mesmo nome.
-
+# 'Coins' foi passado no comando e ele entende que será adicionado nesta tabela.
+# O 'mining_type' ele sabe que é outra tabela pois ele tem o mesmo nome do model, só que no plural.
 class AddMiningTypeToCoins < ActiveRecord::Migration[7.0]
   def change
     add_reference :coins, :mining_type, foreign_key: true
